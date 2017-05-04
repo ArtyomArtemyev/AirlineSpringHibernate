9beac0b92b6e8418342fa3d3387165dbbbec3899
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-05 00:20:50 +0300

    feature(service) add class for service exception

---
+++ src/main/java/by/netcracker/artemyev/exception/ServiceException.java
@@ -0,0 +1,37 @@
+package by.netcracker.artemyev.exception;
+
+/**
+ * Class describes a generic service exception
+ * @autor Artemyev Artoym
+ */
+public class ServiceException extends Exception {
+
+    /**
+     * Constructs a ServiceException with the given detail message.
+     *
+     * @param message The detail message of the ServiceException.
+     */
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a ServiceException with the given root cause.
+     *
+     * @param cause The root cause of the ServiceException.
+     */
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a ServiceException with the given detail message and root cause.
+     *
+     * @param message The detail message of the ServiceException.
+     * @param cause   The root cause of the ServiceException.
+     */
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
