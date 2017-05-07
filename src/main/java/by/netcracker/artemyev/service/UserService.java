e238b7ab74871e29bef42726abdbb53a2891a5e5
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-07 23:23:15 +0300

    feature(service) feature user service add function for authorization user

--- src/main/java/by/netcracker/artemyev/service/UserService.java
+++ src/main/java/by/netcracker/artemyev/service/UserService.java
@@ -1,8 +1,27 @@
 package by.netcracker.artemyev.service;
 
+import by.netcracker.artemyev.dao.UserDao;
 import by.netcracker.artemyev.entity.User;
+import by.netcracker.artemyev.util.UserRoleChecker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service("userService")
 public class UserService extends GenericService<User> {
+
+    @Autowired
+    private UserDao userDAO;
+
+    public String checkUser(String userLogin, String userPassword) {
+        String namePage = "errorAuthorization";
+        List<User> userList = userDAO.getByLoginAndPassword(userLogin, userPassword);
+        if(userList.size() != 0) {
+            return UserRoleChecker.defineUserPage(userList.get(0));
+        }
+        return namePage;
+    }
+
 }
