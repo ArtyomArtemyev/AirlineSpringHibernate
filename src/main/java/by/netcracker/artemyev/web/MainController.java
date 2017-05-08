4cac0393d53ffb2ad188472c568ecb5422d7fb3c
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-08 21:53:11 +0300

    refactoring(MainController)

--- src/main/java/by/netcracker/artemyev/web/MainController.java
+++ src/main/java/by/netcracker/artemyev/web/MainController.java
@@ -1,5 +1,6 @@
 package by.netcracker.artemyev.web;
 
+import by.netcracker.artemyev.constant.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,21 +12,21 @@ public class MainController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView getIndex() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName(Page.INDEX);
         return modelAndView;
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public ModelAndView getRegistration() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("registration");
+        modelAndView.setViewName(Page.REGISTRATION);
         return modelAndView;
     }
 
     @RequestMapping(value = "/authorization", method = RequestMethod.GET)
     public ModelAndView getAuthorization() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("authorization");
+        modelAndView.setViewName(Page.AUTHORIZATION);
         return modelAndView;
     }
 
