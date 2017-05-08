32fd646fda41f6cf9dcb3e740579a335e96481cd
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-08 21:52:06 +0300

    feature(util) add class for handling error

---
+++ src/main/java/by/netcracker/artemyev/util/ErrorHandler.java
@@ -0,0 +1,14 @@
+package by.netcracker.artemyev.util;
+
+import by.netcracker.artemyev.constant.Page;
+import org.apache.log4j.Logger;
+
+public class ErrorHandler {
+
+    private static Logger logger = Logger.getLogger(ErrorHandler.class.getName());
+
+    public static String returnErrorPage(String error, String className) {
+        logger.error(className + ": " + error);
+        return Page.ERROR;
+    }
+}
