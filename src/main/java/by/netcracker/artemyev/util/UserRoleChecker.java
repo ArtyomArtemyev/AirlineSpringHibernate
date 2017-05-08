abbe5e4ec796b70e67158f70fb74ccfd22037ed9
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-08 21:52:39 +0300

    refactoring(UserRoleChecker)

--- src/main/java/by/netcracker/artemyev/util/UserRoleChecker.java
+++ src/main/java/by/netcracker/artemyev/util/UserRoleChecker.java
@@ -1,23 +1,21 @@
 package by.netcracker.artemyev.util;
 
+import by.netcracker.artemyev.constant.Page;
 import by.netcracker.artemyev.entity.User;
 
 public final class UserRoleChecker {
-    private static final String USER_PAGE = "userIndex";
-    private static final String ADMIN_PAGE = "administratorIndex";
-    private static final String DISPATCHER_PAGE = "dispatcherIndex";
 
     public static String defineUserPage(User user) {
         String namePage = "errorAuthorization";
         switch (user.getRole().getRoleType()) {
             case USER:
-                namePage = USER_PAGE;
+                namePage = Page.USER_INDEX;
                 break;
             case DISPATCHER:
-                namePage = ADMIN_PAGE;
+                namePage = Page.ADMIN_INDEX;
                 break;
             case ADMINISTRATOR:
-                namePage = DISPATCHER_PAGE;
+                namePage = Page.DISPATCHER_INDEX;
                 break;
         }
         return namePage;
