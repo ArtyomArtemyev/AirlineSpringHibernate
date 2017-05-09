6da2f56f41b713f8e2ec25f5a07db051847f5ed4
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-10 01:53:49 +0300

    refactoring(ErrorMessage)

--- src/main/java/by/netcracker/artemyev/constant/ErrorMessage.java
+++ src/main/java/by/netcracker/artemyev/constant/ErrorMessage.java
@@ -5,10 +5,10 @@ package by.netcracker.artemyev.constant;
  * @autor Artemyev Artoym
  */
 public final class ErrorMessage {
-    public static final String MESSAGE_ADD_ENTITY_FAIL = "Add was failed";
-    public static final String MESSAGE_UPDATE_ENTITY_FAIL = "Update was failed";
-    public static final String MESSAGE_REMOVE_ENTITY_FAIL = "Remove was failed";
-    public static final String MESSAGE_GET_BY_ID_ENTITY_FAIL = "Get by id was failed";
-    public static final String MESSAGE_GET_ALL_ENTITY_FAIL = "Get all was failed";
-    public static final String MESSAGE_GET_ENTITY_BY_LOGIN_AND_PASSWORD_FAIL = "Get by login and password was failed";
+    public static final String ADD_ENTITY_FAIL = "Add was failed";
+    public static final String UPDATE_ENTITY_FAIL = "Update was failed";
+    public static final String REMOVE_ENTITY_FAIL = "Remove was failed";
+    public static final String GET_BY_ID_ENTITY_FAIL = "Get by id was failed";
+    public static final String GET_ALL_ENTITY_FAIL = "Get all was failed";
+    public static final String GET_ENTITY_BY_LOGIN_AND_PASSWORD_FAIL = "Get by login and password was failed";
 }
