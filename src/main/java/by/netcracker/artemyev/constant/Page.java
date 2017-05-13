2e7962b6cf8df03b62682138c1a9618376ed0f83
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-14 00:18:38 +0300

    fix(constant) fix reference to successfulRegistration page

--- src/main/java/by/netcracker/artemyev/constant/Page.java
+++ src/main/java/by/netcracker/artemyev/constant/Page.java
@@ -8,7 +8,7 @@ public final class Page {
     public static final String INDEX = "index";
     public static final String REGISTRATION = "registration";
     public static final String AUTHORIZATION = "authorization";
-    public static final String SUCCESSFUL_REGISTRATION = "successfullRegistration";
+    public static final String SUCCESSFUL_REGISTRATION = "successfulRegistration";
     public static final String USER_INDEX = "userIndex";
     public static final String ADMIN_INDEX = "administratorIndex";
     public static final String DISPATCHER_INDEX = "dispatcherIndex";
