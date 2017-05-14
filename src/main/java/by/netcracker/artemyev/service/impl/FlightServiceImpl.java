244bb2fc05ba4dfb2e304b8ef0bf6d9682be216a
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-14 20:26:59 +0300

    feature(FlightServiceImpl) add method for add new flight

--- src/main/java/by/netcracker/artemyev/service/impl/FlightServiceImpl.java
+++ src/main/java/by/netcracker/artemyev/service/impl/FlightServiceImpl.java
@@ -2,10 +2,12 @@ package by.netcracker.artemyev.service.impl;
 
 import by.netcracker.artemyev.dao.FlightDao;
 import by.netcracker.artemyev.entity.impl.Flight;
+import by.netcracker.artemyev.exception.ServiceException;
 import by.netcracker.artemyev.service.FlightService;
 import by.netcracker.artemyev.service.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @autor Artemyev Artoym
@@ -15,5 +17,12 @@ public class FlightServiceImpl extends GenericService<Flight> implements FlightS
 
     @Autowired
     private FlightDao flightDao;
+
+    @Transactional
+    @Override
+    public void addFlight(String navigation) throws ServiceException {
+        Flight flight = new Flight(navigation, null);
+        this.add(flight);
+    }
 
 }
