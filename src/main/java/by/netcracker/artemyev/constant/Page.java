22679e0af46a0e798ac90743318640aa515e8cdf
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-14 20:25:28 +0300

    feature(Page) add reference to  admin page and successful add page

--- src/main/java/by/netcracker/artemyev/constant/Page.java
+++ src/main/java/by/netcracker/artemyev/constant/Page.java
@@ -12,6 +12,8 @@ public final class Page {
     public static final String USER_INDEX = "userIndex";
     public static final String USER_MAIN = "userMain";
     public static final String ADMIN_INDEX = "administratorIndex";
+    public static final String ADMIN_MAIN = "administratorMain";
     public static final String DISPATCHER_INDEX = "dispatcherIndex";
     public static final String ERROR = "error";
+    public static final String SUCCESSFUL_ADD_FLIGHT = "successfulAddFlight";
 }
