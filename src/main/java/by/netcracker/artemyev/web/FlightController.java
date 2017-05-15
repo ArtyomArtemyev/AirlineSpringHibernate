2261b54822c5fcbda6bcee7f440053c241eadb4c
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-15 23:42:12 +0300

    feature(FlightController) add fuction for delete flight

--- src/main/java/by/netcracker/artemyev/web/FlightController.java
+++ src/main/java/by/netcracker/artemyev/web/FlightController.java
@@ -8,8 +8,10 @@ import by.netcracker.artemyev.util.ErrorHandler;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -66,6 +68,18 @@ public class FlightController {
         }
         modelAndView.setViewName(returnPage);
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/flight/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String deleteFlight(@PathVariable("id") String id) {
+        String returnText = "Flight was deleted successful";
+        try {
+            flightService.deleteFlight(Long.parseLong(id));
+        }  catch (ServiceException e) {
+            logger.debug(e);
+        }
+        return returnText;
     }
 
 }
