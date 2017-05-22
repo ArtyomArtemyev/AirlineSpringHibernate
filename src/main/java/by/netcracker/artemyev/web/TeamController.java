e61d5ad372ef0812b21bac309ee55fdbbcb8f588
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-22 13:16:21 +0300

    feature(controller) add team controller

---
+++ src/main/java/by/netcracker/artemyev/web/TeamController.java
@@ -0,0 +1,11 @@
+package by.netcracker.artemyev.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @autor Artemyev Artoym
+ */
+@Controller
+public class TeamController {
+    private final String prefix = "/airline/";
+}
