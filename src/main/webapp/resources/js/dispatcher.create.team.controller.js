117daeb6d0ee3914cdd9ac2dfd95c9db62b0dedb
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-24 01:23:44 +0300

    fix(dispatcher.create.team.controller) fix bug with correct structure of team

--- src/main/webapp/resources/js/dispatcher.create.team.controller.js
+++ src/main/webapp/resources/js/dispatcher.create.team.controller.js
@@ -1,7 +1,6 @@
 (function () {
 
     var sendButton = document.getElementById('createTeamButton');
-
     sendButton.addEventListener('click',sendButtonClickHandler);
 
     var result;
@@ -21,10 +20,9 @@
             }
         }
 
-        //Проверка правильности комманды
         var pilot = [];
-        var navigator;
-        var radio_operator;
+        var navigator = [];
+        var radio_operator = [];
         var stewardess = [];
         for(var i = 0; i < appointmentMembers.length; i++) {
             switch (appointmentMembers[i]) {
@@ -33,11 +31,11 @@
                     break;
                 }
                 case "NAVIGATOR": {
-                    navigator = true;
+                    navigator.push(1);
                     break;
                 }
                 case "RADIO_OPERATOR": {
-                    radio_operator = true;
+                    radio_operator.push(1);
                     break;
                 }
                 case "STEWARDESS": {
@@ -47,7 +45,7 @@
             }
         }
 
-        if(pilot.length === 2 && navigator && radio_operator && stewardess.length === 2) {
+        if(pilot.length === 2 && navigator.length === 1 && radio_operator.length === 1 && stewardess.length === 2) {
 
             for(var i = 0; i < idMembers.length; i++) {
                 if(i === 0) {
@@ -66,17 +64,26 @@
         }
         else {
             alert("Team consist of 1 navigator and 1 radio operator, 2 pliots and 2 stewardess" + "\n" + "Please make true team");
+
+            idMembers = [];
+            for (var i = 1; i < employeeList.length - 1; i++) {
+                var td = employeeList[i].querySelectorAll("td")[5];
+                var checkbox = td.querySelector("input[type='checkbox']");
+                if (checkbox.checked) {
+                    checkbox.checked='';
+                }
+            }
         }
 
     }
     
     function sendTeamData() {
         var prefix = '/airline/';
-        console.log(JSON.stringify(idMembers));
+        console.log(result);
         $.ajax({
             type: 'POST',
             url: prefix +'team',
-            data: JSON.stringify(idMembers),
+            data: result,
             headers: {
                 'Accept': 'application/text',
                 'Content-Type': 'application/json'
