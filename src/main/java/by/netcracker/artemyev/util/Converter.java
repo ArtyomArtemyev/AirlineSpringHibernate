e17f8f3998f216d651924a5f618e698da02141fa
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-24 23:21:35 +0300

    feature(Converter) rename function, add logging and describing

--- src/main/java/by/netcracker/artemyev/util/Converter.java
+++ src/main/java/by/netcracker/artemyev/util/Converter.java
@@ -1,7 +1,10 @@
 package by.netcracker.artemyev.util;
 
+import by.netcracker.artemyev.constant.LoggingName;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -9,23 +12,27 @@ import java.util.Collection;
 import java.util.List;
 
 /**
+ * Class describes converter for different types
  * @autor Artemyev Artoym
  */
 public class Converter {
+    private static Logger logger = LogManager.getLogger(Converter.class.getName());
     private static Gson gson = new Gson();
 
-    public static List<Long> fromWebJson(String jsonLine) {
-        String idLine = jsonLine.replaceAll("undefined","");
+    public static List<Long> convertToList(String line) {
+        logger.debug(LoggingName.FUNCTION_CONVERSION_TO_LIST);
+        String idLine = line.replaceAll("undefined","");
         Type type = new TypeToken<Collection<Long>>(){}.getType();
         List<Long> list = new ArrayList<>();
         list = gson.fromJson(idLine, type);
         return list;
     }
 
-    public static String toJson(List<Long> list) {
-        String jsonLine = null;
-        jsonLine = gson.toJson(list);
-        return jsonLine;
+    public static String convertToLine(List<Long> list) {
+        logger.debug(LoggingName.FUNCTION_CONVERSION_TO_LINE);
+        String line = null;
+        line = gson.toJson(list);
+        return line;
     }
 
 
