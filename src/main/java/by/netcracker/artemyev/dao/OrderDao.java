1211f2f0edadcc9f4e3903a2fc188fe42110ca94
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-05-29 13:41:44 +0300

    feature(dao) add dao for entity order

---
+++ src/main/java/by/netcracker/artemyev/dao/OrderDao.java
@@ -0,0 +1,6 @@
+package by.netcracker.artemyev.dao;
+
+import by.netcracker.artemyev.entity.impl.Order;
+
+public interface OrderDao extends GeneralDao<Order> {
+}
---
+++ src/main/java/by/netcracker/artemyev/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,33 @@
+package by.netcracker.artemyev.dao.impl;
+
+import by.netcracker.artemyev.constant.ErrorMessage;
+import by.netcracker.artemyev.constant.Statement;
+import by.netcracker.artemyev.dao.GenericDao;
+import by.netcracker.artemyev.dao.OrderDao;
+import by.netcracker.artemyev.entity.impl.Order;
+import by.netcracker.artemyev.exception.DaoException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.HibernateException;
+
+import java.util.List;
+
+public class OrderDaoImpl extends GenericDao<Order> implements OrderDao {
+    private static Logger logger = LogManager.getLogger(OrderDaoImpl.class.getName());
+
+    private OrderDaoImpl() {
+        super(Order.class);
+    }
+
+    @Override
+    public List<Order> getAll() throws DaoException {
+        List<Order> orderList;
+        try {
+            orderList = getEntityManager().createQuery(Statement.GET_ALL_ORDERS).getResultList();
+        } catch (HibernateException e) {
+            logger.debug(e);
+            throw new DaoException(ErrorMessage.GET_ALL_ENTITY_FAIL, e);
+        }
+        return orderList;
+    }
+}
