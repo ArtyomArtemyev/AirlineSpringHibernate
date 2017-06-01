07a32717bc987b75e46c6f9ad4f0f8b7afda982e
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-06-01 23:43:42 +0300

    feature(dispatcher.appoint.team.controller) add sending form to server

--- src/main/webapp/resources/js/dispatcher.appoint.team.controller.js
+++ src/main/webapp/resources/js/dispatcher.appoint.team.controller.js
@@ -1,20 +1,12 @@
 (function () {
 
-    var sendButton = document.getElementById('appointButton');
+    var sendButton = document.getElementById('createTeamButton');
     sendButton.addEventListener('click',sendButtonClickHandler);
 
-    var teamId = 0;
     var flightId = 0;
 
     function sendButtonClickHandler(event) {
         event.preventDefault();
-
-        var teamRadioButton = document.getElementsByClassName('radioButtonTeam');
-        for (var i = 0; i < teamRadioButton.length; i++) {
-            if (teamRadioButton[i].type == "radio" && teamRadioButton[i].checked) {
-                teamId = teamRadioButton[i].value;
-            }
-        }
 
         var flightRadioButton = document.getElementsByClassName('radioButtonFlight');
         for (var j = 0; j < flightRadioButton.length; j++) {
@@ -23,36 +15,16 @@
             }
         }
 
-        if(flightId !== 0 && teamId !== 0) {
+        if(flightId !== 0 ) {
 
-            sendUpdateFlight();
+            var sendForm = document.getElementById('changeForm');
+            sendForm.action ="flight/update/"+flightId;
+            sendForm.submit();
         }
         else {
             alert("Please check team and flight and then click on button");
         }
 
-    }
-
-    function sendUpdateFlight() {
-        var prefix = '/airline/';
-        $.ajax({
-            type: 'PUT',
-            url: prefix +'teams/' + teamId,
-            data: flightId,
-            headers: {
-                'Accept': 'application/text',
-                'Content-Type': 'application/json'
-            },
-            dataType: 'text',
-            success: function(receive) {
-                $("#contentDiv").empty();
-                $("#informationP").replaceWith(receive);
-                $("#hiddenLi").removeAttr('style');
-            },
-            error: function (jqXhr, textStatus, errorThrown) {
-                alert("Ошибка '" + jqXhr.status + "' (textStatus: '" + textStatus + "', errorThrown: '" + errorThrown + "')");
-            }
-        });
     }
 
 })();
