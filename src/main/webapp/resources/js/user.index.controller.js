c78174820ed348128329bf4d8c2280500868499a
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-06-04 02:04:26 +0300

    feature(user.index.controller) add removing item for user in local storage

--- src/main/webapp/resources/js/user.index.controller.js
+++ src/main/webapp/resources/js/user.index.controller.js
@@ -1,7 +1,8 @@
  function setId(event) {
     event.preventDefault();
 
-     var inputId = document.getElementById('idUser')
+     var inputId = document.getElementById('idUser');
+     localStorage.removeItem('id');
      localStorage.setItem('id', inputId.value);
      inputId.remove();
  }
