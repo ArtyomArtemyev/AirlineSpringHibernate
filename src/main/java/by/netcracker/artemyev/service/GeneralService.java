2673f6bfaa7cd49d538ec46ebf8be26135554132
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-06-04 14:54:30 +0300

    feature(GeneralService) add some comments

--- src/main/java/by/netcracker/artemyev/service/GeneralService.java
+++ src/main/java/by/netcracker/artemyev/service/GeneralService.java
@@ -5,12 +5,50 @@ import by.netcracker.artemyev.exception.ServiceException;
 import java.util.List;
 
 /**
+ * This interface describes common service
+ *
  * @autor Artemyev Artoym
  */
 public interface GeneralService<T> {
+
+    /**
+     * Create the given Entity
+     *
+     * @param  - entity to be created
+     * @throws ServiceException - if fails with adding at Service layer
+     */
     void add(T object) throws ServiceException;
+
+    /**
+     * Update the given Entity
+     *
+     * @param  - entity to be updated
+     * @throws ServiceException - if fails with adding at Service layer
+     */
     void update(T object) throws ServiceException;
+
+    /**
+     * Update the given Entity
+     *
+     * @param  - entity to be deleted
+     * @throws ServiceException - if fails with adding at Service layer
+     */
     void remove(T object) throws ServiceException;
+
+    /**
+     * Update the given Entity
+     *
+     * @param  - id of the Entity to be returned
+     * @return - the Entity matching the given id
+     * @throws ServiceException - if fails with adding at Service layer
+     */
     T getById(Long id) throws ServiceException;
+
+    /**
+     * Returns a list of all Entities
+     *
+     * @return a list of all Entities
+     * @throws ServiceException - if something fails at Service layer
+     */
     List<T> getAll() throws ServiceException;
 }
