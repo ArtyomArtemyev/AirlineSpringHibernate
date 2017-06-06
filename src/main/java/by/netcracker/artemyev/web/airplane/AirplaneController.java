169a54f0dd318c62ba655aea2f34faae82c93da9
Artyom Artemyev <artyomartemyev962015@gmail.com>
2017-06-07 00:11:58 +0300

    refactoring(AirplaneController)

--- src/main/java/by/netcracker/artemyev/web/airplane/AirplaneController.java
+++ src/main/java/by/netcracker/artemyev/web/airplane/AirplaneController.java
@@ -1,5 +1,6 @@
 package by.netcracker.artemyev.web.airplane;
 
+import by.netcracker.artemyev.constant.LoggingName;
 import by.netcracker.artemyev.constant.ServerResponse;
 import by.netcracker.artemyev.exception.ServiceException;
 import by.netcracker.artemyev.service.AirplaneService;
@@ -26,28 +27,31 @@ public class AirplaneController {
     private FlightService flightService;
 
     @RequestMapping(value = "/{id}/team", method = RequestMethod.GET)
-    public @ResponseBody String updateFlight(@PathVariable("id") String id) {
-        String returnText = ServerResponse.EDIT_FLIGHT;
+    @ResponseBody
+    public String updateFlight(@PathVariable("id") String id) {
+        logger.debug(LoggingName.CONTROLLER_UPDATE_FLIGHT);
+        String returnText = ServerResponse.EDITED_FLIGHT;
         try {
             returnText = airplaneService.getTypeTeam(Long.valueOf(id));
         } catch (ServiceException e) {
-            logger.debug(e);
+            logger.error(e);
         }
         return returnText;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String appointAirplane(@PathVariable("id") String id, @RequestBody String json) {
-        String returnText = ServerResponse.APPOINT_AIRPLANE;
+    @ResponseBody
+    public  String appointAirplane(@PathVariable("id") String id, @RequestBody String json) {
+        logger.debug(LoggingName.CONTROLLER_APPOINT_AIRPLANE);
+        String returnText = ServerResponse.NO_APPOINT_AIRPLANE;
         if(DataChecker.checkUserData(json)) {
             JSONObject jsonObject = new JSONObject(json);
             try {
                 flightService.appointAirplane(Long.valueOf(jsonObject.getString(FIELD_ID_FLIGHT)), Long.valueOf(id));
+                returnText = ServerResponse.APPOINT_AIRPLANE;
             } catch (ServiceException e) {
-                logger.debug(e);
+                logger.error(e);
             }
-        } else {
-            returnText = ServerResponse.NO_APPOINT_AIRPLANE;
         }
         return returnText;
     }
